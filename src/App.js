2e9503be793228afc47689b6bc04d53c93410a7b
jovita <jovitaamelinda05@gmail.com>
2022-09-22 12:11:31 +0800

    add and modified file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-
 import './App.css';
-import logo from "./logo.png";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Carousel from './components/Carousel'; 
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 function App() {
   return  (
     <div>
-      <img src='https://images.glints.com/unsafe/180x0/glints-dashboard.s3.amazonaws.com/company-logo/25f6ffcefbac1f1d53d01f97f7a264da.png' alt="" width="60" height="60" class="d-inline-block" />
-      <h1><a href='#'>Rise Store</a></h1>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        
-                
-                
-            
+      <Router> 
+        <Header /> 
+        <Footer />  
+        <Carousel /> 
+      </Router>      
     </div>
-
   )
 }
+
 
 
 export default App;
